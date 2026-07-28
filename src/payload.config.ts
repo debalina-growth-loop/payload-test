00323e9f4931b83e103c1327f4e4581dfdd371be
abhijit_dev <abhijitghanta@hotmail.com>
2026-07-28 14:44:50 +0530

    fixed admin panel  pages route list data

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -61,7 +61,8 @@ export default buildConfig({
   editor: defaultLexical,
   db: postgresAdapter({
     pool: {
-      connectionString: process.env.NEON_URL,
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
     },
   }),
   collections: [Pages, Posts, Products, Media, Categories, Users],
